4cb16bc9a29d4b614afe4126ed1a794f8bdaf859
nathannli <nathan.li.1995@gmail.com>
2017-02-04 02:30:42 -0500

    added profile button listener

--- QHacks/app/src/main/java/com/example/android/qhacks/Home.java
+++ QHacks/app/src/main/java/com/example/android/qhacks/Home.java
@@ -1,18 +1,33 @@
 package com.example.android.qhacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
     //testing push
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         final ImageView homeButton = (ImageView) findViewById(R.id.homeImageView);
+
         final ImageView profileButton = (ImageView) findViewById(R.id.profileImageView);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), UserProfile.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
         final ImageView searchButton = (ImageView) findViewById(R.id.searchImageView);
-        
+
+
+
+
     }
 }
