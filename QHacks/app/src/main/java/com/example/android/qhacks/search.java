88805d060fa771a3553b640f93b750f16b70338d
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-02-04 03:22:11 -0500

    Modified search and user profile activities

--- QHacks/app/src/main/java/com/example/android/qhacks/search.java
+++ QHacks/app/src/main/java/com/example/android/qhacks/search.java
@@ -1,7 +1,10 @@
 package com.example.android.qhacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class search extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final ImageView homeButton = (ImageView) findViewById(R.id.homeImageView);
+        homeButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Home.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        final ImageView profileButton = (ImageView) findViewById(R.id.profileImageView);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), UserProfile.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        final ImageView searchButton = (ImageView) findViewById(R.id.searchImageView);
+
     }
 }
