3f1c11c11ac8066afeaefbfff7552c2a521b37c3
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-02-04 00:11:13 -0500

    Added Abstract User class and an Doctor subclass

---
+++ QHacks/app/src/main/java/com/example/android/qhacks/Doctor.java
@@ -0,0 +1,37 @@
+package com.example.android.qhacks;
+
+/**
+ * Created by Gilbert Lam on 2017-02-03.
+ */
+
+public class Doctor extends User {
+    private String[] qualifications;
+    private String fieldOfStudy;
+    public Doctor (String name, String postalCode, String fieldOfStudy, String phoneNumber, String country, String province, String[] qualifications) {
+        this.name = name;
+        this.postalCode = postalCode;
+        this.fieldOfStudy = fieldOfStudy;
+        this.phoneNumber = phoneNumber;
+        this.country = country;
+        this.province = province;
+        this.qualifications = qualifications;
+    }
+    public String getName (){
+        return name;
+    }
+    public String getPostalCode(){
+        return postalCode;
+    }
+    public String getFieldOfStudy(){
+        return fieldOfStudy;
+    }
+    public String getCountry(){
+        return country;
+    }
+    public String getProvince(){
+        return province;
+    }
+    public String[] getQualifications(){
+        return qualifications;
+    }
+}
---
+++ QHacks/app/src/main/java/com/example/android/qhacks/User.java
@@ -0,0 +1,16 @@
+package com.example.android.qhacks;
+
+/**
+ * Created by Gilbert Lam on 2017-02-03.
+ */
+
+public abstract class User {
+    protected String name;
+    protected String phoneNumber;
+    protected String country;
+    protected String province;
+    protected String postalCode;
+    public User(){
+
+    }
+}
