8ee637b4654eba016be72f9327e36731f2c578cb
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-02-05 04:11:09 -0500

    :D

--- QHacks/app/src/main/java/com/example/android/qhacks/Doctor.java
+++ QHacks/app/src/main/java/com/example/android/qhacks/Doctor.java
@@ -21,10 +21,24 @@ public class Doctor extends User {
         return qualifications;
     }
     public boolean search(String search){
-
-        if (qualifications.contains(search) || name.equals(search) || phoneNumber.equals(search) || country.equals(search) || province.equals(search) || age.equals(search) || email.equals(search)){
+        System.out.println(qualifications+ " "+name +" " + " "+age +" " +phoneNumber+" "+country+" " +province+ " hey");
+        System.out.println(qualifications.getClass());
+        System.out.println(search.getClass());
+            if (qualifications.contains(search) || search.equals(phoneNumber) || search.equals(country) || search.equals(name) || search.equals(province) || search.equals(age) || search.equals(email))
             return true;
-        }
         return false;
+                /*if (search.equals(phoneNumber))
+                    System.out.println("a");
+                    if(country.equals(search))
+                        System.out.println("a");
+                        if(province.equals(search))
+                            System.out.println("a");
+                            if(age.equals(search))
+                                System.out.println("a");
+                                if(email.equals(search))
+                                    return true;*/
+
+       // System.out.println(name + " hi");return false;
     }
+
 }
